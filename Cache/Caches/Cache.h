a8be82828c2e0d1fd5ccaaa0fbb3f9cb6c929499
code_cat <kristina.kurova@protonmail.com>
2017-03-20 02:57:18 +0300

    Change log style, using pragma instead of #ifndef/#define.

--- Cache/Caches/Cache.h
+++ Cache/Caches/Cache.h
@@ -1,15 +1,14 @@
 //
 // Created by cat on 9/25/16.
 //
+
 #pragma once
-#include <list>
+#include <iostream>
 #include <unordered_map>
-#include <fstream>
+#include <list>
 #include "../Generators/Request.h"
-#include "../Utils/Utils.h"
 #include "../Utils/Types.h"
-#ifndef CACHE_CACHE_H
-#define CACHE_CACHE_H
+
 typedef std::list<Request> StorType;
 
 class Cache
@@ -18,70 +17,62 @@ protected:
     double _hit;
     double _miss;
 
-    /**
-     * [Maximal capacity of cache]
-     */
+    /// [Maximal capacity of cache]
     ByteSize _max_capacity;
 
-    /**
-     * [Average stack distance in all experiments]
-     */
+    ///[Average stack distance in all experiments]
     StackDist _avg_stack_dist;
+
+    /// [Inner storage]
     StorType _list_store;
 
 public:
-    /**
-     * [Number of requests since experiment has begun]
-     */
+    /// [Number of requests since experiment has begun]
     int _request_counter;
 
-    /**
-     * [Average hit rate of requests]
-     */
+    /// [Average hit rate of requests]
     HitRate _hit_rate;
 
-    /**
-     * [Current capaсity of the cache]
-     */
+    /// [Current capaсity of the cache]
     ByteSize _curr_capacity;
 
-    /**
-     * [Current number of element]
-     */
+    /// [Current number of element]
     ByteSize _curr_size;
-    /**
-     * [Storage, contains logical block address as key and
-     * iterator pointed to corresponding request body as value]
-     */
+
+     /// [Storage, contains logical block address as key and
+     /// iterator pointed to corresponding request body as value]
     std::unordered_map<Lba, StorType::iterator> _map_store;
 
-    /**
-     * [Default constructor]
-     */
+    ///
     Cache();
 
-    /**
-     * [Constructor]
-     * @param capaсity [Specifies the common cache size (_max_capaсity).]
-     */
+    ///
+    /// \param capacity [Specifies the common cache size (_max_capaсity)]
     Cache(ByteSize capacity);
 
-    /**
-     * [Virtual destructor]
-     */
+    ///
     virtual ~Cache(){};
 
-    /**
-     * [Checks, if request has already been in cache.]
-     * @param cell_address
-     * @return [true if request is already existed in cache and false otherwise]
-     */
+     /// \brief                 [Checks, if request has already been in cache.]
+     /// \param cell_address
+     /// \param it
+     /// \return                [true if request is already existed in cache and false otherwise]
     bool IsInCache(Lba cell_address, std::unordered_map<Lba, StorType::iterator>::iterator &it);
+
+    ///
+    /// \param request_size
+    /// \return
     bool IsCacheFull(ByteSize request_size) const;
+
+    ///
+    /// \param new_capasity
     void ChangeCacheCapasity(ByteSize new_capasity);
+
+    ///
+    /// \return
     HitRate CalculateHitRate();
+
+    ///
+    /// \return
     StackDist CalculateAvgStackDistance();
 };
-
-
-#endif //CACHE_CACHE_H
