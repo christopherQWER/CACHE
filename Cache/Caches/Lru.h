8ce47e4595ad988092e6da5a8555557e9c46b12e
code_cat <kristina.kurova@protonmail.com>
2017-03-20 02:51:58 +0300

    Change documentation style (from C to C++-style).

--- Cache/Caches/Lru.h
+++ Cache/Caches/Lru.h
@@ -1,33 +1,32 @@
 //
 // Created by cat on 9/25/16.
 //
+
 #pragma once
-#include <ctime>
 #include "Cache.h"
-#include "../Utils/Utils.h"
-#include "../Loggers/Logger.h"
-#ifndef CACHE_LRU_H
-#define CACHE_LRU_H
 
 class Lru : public Cache
 {
 public:
-    /**
-     * [Constructor]
-     * @param capacity [cache capasity]
-     */
+    /// \brief
+    /// \param capacity []
     Lru(ByteSize capacity);
 
-    /**
-     * [Destructor]
-     */
+    ///
     ~Lru();
 
+    ///
+    /// \param newRequest []
     void LRU(Request &newRequest);
+
+    ///
+    /// \param newRequest []
     void ReorganizeCache(const Request &newRequest);
+
+    ///
+    /// \param newRequest []
     void InsertNewRequest(const Request &newRequest);
+
+    ///
     void DeleteOldRequest();
 };
-
-
-#endif //CACHE_LRU_H
