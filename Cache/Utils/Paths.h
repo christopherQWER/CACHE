15fbd9fe97a479f31bbc8c08bc275292dbbeb1ec
code_cat <kristina.kurova@protonmail.com>
2017-05-23 01:46:31 +0300

    Delete unused path.

--- Cache/Utils/Paths.h
+++ Cache/Utils/Paths.h
@@ -2,19 +2,8 @@
 // Created by cat on 12/3/16.
 //
 
-/// Trace log paths
-#define _WEB_SEARCH_1_      "//home//cat//Documents//CACHE//Tests//Traces//WebSearch//WebSearch1.spc"
-#define _WEB_SEARCH_2_      "//home//cat//Documents//CACHE//Tests//Traces//WebSearch//WebSearch2.spc"
-#define _WEB_SEARCH_3_      "//home//cat//Documents//CACHE//Tests//Traces//WebSearch//WebSearch3.spc"
-#define _FINANCIAL_1_       "//home//cat//Documents//CACHE//Tests//Traces//Financial//Financial1.spc"
-#define _FINANCIAL_2_       "//home//cat//Documents//CACHE//Tests//Traces//Financial//Financial2.spc"
-
 #define _PDF_DIR_           "PDF"
 #define _CDF_DIR_           "CDF"
 
-#define _PLOT_DATA_         "//home//cat//Documents//CACHE//Tests//PlotData"
-#define _PARETO_            "//home//cat//Documents//CACHE//Tests//Pareto"
-#define _TRACES_DIR_        "//home//cat//Documents//CACHE//Tests//Traces"
 #define _READY_PDF          "//home//cat//Documents//CACHE//Tests//Pdf"
-
 #define _XML_CONFIG_        "//home//cat//Documents//CACHE//Cache//Configs//Config.xml"
