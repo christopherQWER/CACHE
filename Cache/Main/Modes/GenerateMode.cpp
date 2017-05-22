8eaa1f1e7b263609b4ef63f715ebedeb784cd14c
code_cat <kristina.kurova@protonmail.com>
2017-05-23 01:39:00 +0300

    Fixed errors in Simulate mode and implemented Generate mode.

--- Cache/Main/Modes/GenerateMode.cpp
+++ Cache/Main/Modes/GenerateMode.cpp
@@ -14,5 +14,17 @@ void RunGenerateMode(Config my_config)
     XmlGenerateMode::Deserialize(local_config, xmlGenerator);
 
     Logger *logger = Logger::CreateLogger(xmlGenerator.logger.logger_type);
-    Flow flow;
+    Flow flow = StackDistFlow(xmlGenerator.flow.app_count,
+                                xmlGenerator.input_pdf_dir,
+                                xmlGenerator.system_time);
+    string output_trace_file = Utils::PathCombine(xmlGenerator.flow.path_to_flow,
+            Utils::GetCurrentStringUnixTime() + ".spc");
+
+    Request req = flow.GetRequest();
+    while (xmlGenerator.system_time > req._timestamp)
+    {
+        string request_string = RequestParser::GetStringFromRequest(req);
+        Utils::AppendToFile(output_trace_file, request_string);
+        flow.GetRequest();
+    }
 }
--- Cache/Main/Modes/SimulateMode.cpp
+++ Cache/Main/Modes/SimulateMode.cpp
@@ -5,27 +5,43 @@
 #include "ModeRunner.h"
 using namespace std;
 
+// Prototypes
+void GetModeConfig(const string& mode_config_path, XmlSimulate& xmlSimulator);
+Flow* CreateFlowInst(const XmlSimulate& xmlSimulator, const string& input_pdf);
+void CreateXmlAppList(const XmlSimulate& xmlSimulator, const string& analyze_conf_path);
+
+
 void RunSimulateMode(Config my_config)
 {
-    string path_to_config = my_config.simulate_path;
-    pugi::xml_document local_config;
-    Flow *flow;
     XmlSimulate xmlSimulator;
-    ClientsManager *clientsManager;
-    MainConfig::LoadFromFile(path_to_config, local_config);
-    XmlSimulateMode::Deserialize(local_config, xmlSimulator);
+    GetModeConfig(my_config.simulate_path, xmlSimulator);
 
     Logger *logger = Logger::CreateLogger(xmlSimulator.logger.logger_type);
-    switch (xmlSimulator.flow.flow_type)
+    Flow *flow = CreateFlowInst(xmlSimulator, "");
+
+    // Create storage instances and some needed info
+    switch (xmlSimulator.stor_type)
     {
-        case FFILE:
+        case SHARED:
         {
-            flow = new TraceFileFlow(xmlSimulator.flow.path_to_flow);
+            SharedStorage sharedCache = SharedStorage(xmlSimulator.common_size,
+                    xmlSimulator.plot_dir, 60, 0);
+            CreateXmlAppList(xmlSimulator, my_config.analyze_path);
+            ClientsManager clientsManager = ClientsManager(xmlSimulator.app_list);
+            sharedCache.CreateStorage();
+            sharedCache.Run(clientsManager.clients_map, logger, flow, xmlSimulator.with_plots);
+            clientsManager.QosComparator(logger);
             break;
         }
-        case FGENERATOR:
+        case PARTIAL:
         {
-            flow = new StackDistFlow();
+            StaticPartial staticPartial = StaticPartial(xmlSimulator.common_size,
+                    xmlSimulator.plot_dir, 60, 0);
+            CreateXmlAppList(xmlSimulator, my_config.analyze_path);
+            ClientsManager clientsManager = ClientsManager(xmlSimulator.app_list);
+            staticPartial.CreateStorage(xmlSimulator.div_type, clientsManager.clients_map);
+            staticPartial.Run(clientsManager.clients_map, logger, flow, xmlSimulator.with_plots);
+            clientsManager.QosComparator(logger);
             break;
         }
         default:
@@ -33,56 +49,66 @@ void RunSimulateMode(Config my_config)
             break;
         }
     }
+}
 
-    switch (xmlSimulator.stor_type)
+
+void GetModeConfig(const string& mode_config_path, XmlSimulate& xmlSimulator)
+{
+    pugi::xml_document local_config;
+    MainConfig::LoadFromFile(mode_config_path, local_config);
+    XmlSimulateMode::Deserialize(local_config, xmlSimulator);
+}
+
+Flow* CreateFlowInst(const XmlSimulate& xmlSimulator, const string& input_pdf)
+{
+    Flow* flow;
+    switch (xmlSimulator.flow.flow_type)
     {
-        case SHARED:
+    case FFILE:
+    {
+        flow = new TraceFileFlow(xmlSimulator.flow.path_to_flow);
+        break;
+    }
+    case FGENERATOR:
+    {
+        flow = new StackDistFlow(xmlSimulator.flow.app_count, input_pdf);
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+    return flow;
+}
+
+void CreateXmlAppList(const XmlSimulate& xmlSimulator, const string& analyze_conf_path)
+{
+    if (xmlSimulator.app_count == 0)
+    {
+        pugi::xml_document trace_config;
+        XmlTraceAnalyze xmlTraceMode;
+        MainConfig::LoadFromFile(analyze_conf_path, trace_config);
+        XmlAnalyzeMode::Deserialize(trace_config, xmlTraceMode);
+
+        // Get from app units from xml-statistics
+        if (xmlTraceMode.type == DETAILED)
         {
-            SharedStorage sharedCache = SharedStorage(xmlSimulator.common_size,
-                    string(_PLOT_DATA_), 60, 0);
-            sharedCache.CreateStorage();
-            sharedCache.Run();
-            break;
-        }
-        case PARTIAL:
-        {
-            StaticPartial staticPartial = StaticPartial(xmlSimulator.common_size,
-                    string(_PLOT_DATA_), 60, 0);
-            if (xmlSimulator.app_count == 0)
+            string trace_file_dir = Utils::SplitFilename(xmlSimulator.flow.path_to_flow);
+            string source_file = Utils::PathCombine(trace_file_dir,
+                    Utils::GetFileNameWithoutExt(xmlSimulator.flow.path_to_flow) + ".xml");
+
+            TraceInfo config = TraceInfo();
+            pugi::xml_document doc;
+            XmlAnalyzeMode::LoadFromFile(source_file, doc);
+            AnalyzeConfig::Deserialize(doc, config);
+
+            for (const auto &app : config.apps)
             {
-                pugi::xml_document trace_config;
-                XmlTraceAnalyze xmlTraceMode;
-                MainConfig::LoadFromFile(my_config.analyze_path, trace_config);
-                XmlAnalyzeMode::Deserialize(trace_config, xmlTraceMode);
-
-                // Get from app units from xml-statistics
-                if (xmlTraceMode.type == DETAILED)
-                {
-                    string trace_file_dir = Utils::SplitFilename(xmlSimulator.flow.path_to_flow);
-                    string source_file = Utils::PathCombine(trace_file_dir,
-                            Utils::GetFileNameWithoutExt(xmlSimulator.flow.path_to_flow) + ".xml");
-
-                    TraceInfo config = TraceInfo();
-                    pugi::xml_document doc;
-                    XmlAnalyzeMode::LoadFromFile(source_file, doc);
-                    AnalyzeConfig::Deserialize(doc, config);
-
-                    for (const auto &app : config.apps)
-                    {
-                        XmlClient xml_client = XmlClient();
-                        xml_client.asu = app.unit;
-                        xmlSimulator.app_list.push_back(xml_client);
-                    }
-                }
+                XmlClient xml_client = XmlClient();
+                xml_client.asu = app.unit;
+                xmlSimulator.app_list.push_back(xml_client);
             }
-            clientsManager = new ClientsManager(xmlSimulator.app_list);
-            staticPartial.CreateStorage(xmlSimulator.div_type, clientsManager->clients_map);
-            staticPartial.Run(logger, flow, xmlSimulator.with_plots);
-            break;
-        }
-        default:
-        {
-            break;
         }
     }
 }
