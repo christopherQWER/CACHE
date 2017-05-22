b650fb5e3f04fbba3cf9813659dfade24ee52b67
code_cat <kristina.kurova@protonmail.com>
2017-05-23 01:40:50 +0300

    Added includes.

--- Cache/Math/LeastSquareMethod.h
+++ Cache/Math/LeastSquareMethod.h
@@ -1,8 +1,14 @@
 //
 // Created by cat on 5/17/17.
 //
-#include "iostream"
-#include "cmath"
+#include <iostream>
+#include <cmath>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_multifit_nlin.h>
 
 class LeastSquareMethod {
 
